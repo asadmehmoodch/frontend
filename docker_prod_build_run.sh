7f097748b84fc39bf8ef5a56a32b87d7501f4112
Asad Mehmood <amehmood@capsifi.com>
2018-12-02 11:41:31 +1100

    renamed tag to asadmehmoodch

--- docker_prod_build_run.sh
+++ docker_prod_build_run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker build . -t asadmehmood/production
-docker run -p 8080:80 asadmehmood/production
+docker build . -t asadmehmoodch/production
+docker run -p 8080:80 asadmehmoodch/production
